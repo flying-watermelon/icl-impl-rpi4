73f6641d6d062beefdc82f95167878d14ce9d849
flying-watermelon <racnil070512@hotmail.com>
2020-06-20 11:07:53 +0200

    Added status check for initialization.

--- examples/02_spi/main.c
+++ examples/02_spi/main.c
@@ -1,8 +1,12 @@
 #include <ICL/ICL.h>
 #include <ICL/ICL_impl_rpi4.h>
+#include <stdio.h>
 
 int main() {
-    icl_rpi4_initialize(ICL_RPI4_INITIALIZE_SPI);
+    if (icl_rpi4_initialize(ICL_RPI4_INITIALIZE_SPI) != 0) {
+        fprintf(stderr, "Cannot initialize ICL");
+        exit(EXIT_FAILURE);
+    }
 
     icl_rpi4_finalize();
 
